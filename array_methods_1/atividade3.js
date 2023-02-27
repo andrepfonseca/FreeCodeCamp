2f29d39ec2c7fcb886110a989e357d10e1d6f50f
André Fonseca <andre.fonseca@rethink.dev>
2023-02-27 18:17:49 -0300

    Refactoring exercise 3 using reduce

--- array_methods_1/atividade3.js
+++ array_methods_1/atividade3.js
@@ -1,14 +1,8 @@
-const concatenateArrayNumbersIntoString = (arrayOfNumbers) => {
-  let stringOfNumbers = "";
-  let iterator = 0;
-  arrayOfNumbers.forEach((element) => {
-    iterator += 1;
-    stringOfNumbers += element;
-    if (iterator % 3 == 0) {
-      stringOfNumbers += ".";
-    }
-  });
-  return stringOfNumbers;
+const concatenateArrayNumbersIntoString = (array) => {
+  return array.reduce((acc, cur, index) => {
+    if (index % 3 === 0 && index != 0) acc += ".";
+    return (acc += cur);
+  }, "");
 };
 
 console.log(concatenateArrayNumbersIntoString([1, 2, 3, 4, 5, 6, 7, 8]));
